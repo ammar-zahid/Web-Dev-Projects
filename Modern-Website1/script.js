1c3d5c615b314fada78d10c12ff29b167b59a062
Ammar Zahid <131453458+ammar-zahid@users.noreply.github.com>
2023-09-01 12:27:36 +0500

    Mouse Follower and Animations

--- Modern-Website1/script.js
+++ Modern-Website1/script.js
@@ -3,10 +3,73 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-const main = document.querySelector("#main");
 const mouseMove = document.querySelector("#mousecircle");
 
-main.addEventListener('mousemove', function(dets){
+function mouseMoveAnim(x, y){
+
+window.addEventListener('mousemove', function(dets){
     mouseMove.style.left = dets.x+"px";
     mouseMove.style.top = dets.y+"px";
+    mouseMove.style.transform = `scale(${x}, ${y})`;
 })
+}
+
+var timeout;
+
+function circleChaptaKaro(){
+    //define default scale value
+    var xscale = 1;
+    var yscale = 1;
+
+    var xprev = 0;
+    var yprev = 0;
+
+    window.addEventListener("mousemove", function(dets){
+    clearInterval(timeout)
+       var xdiff =  dets.clientX - xprev;
+       var ydiff = dets.clientY - yprev;
+       
+       xscale = gsap.utils.clamp(.8, 1.2, xdiff);
+       yscale = gsap.utils.clamp(.8, 1.2, ydiff);
+
+       xprev = dets.clientX;
+       yprev = dets.clientY;
+
+       mouseMoveAnim(xscale, yscale);
+       timeout = setTimeout(function(){
+        mouseMove.style.transform = `scale(1, 1)`;
+       }, 100)
+    });
+}
+
+function firstPageAnim(){
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+
+    tl.to(".boundingelem", {
+        y: -4,
+        ease: Expo.easeInOut,
+        duration: 2,
+        delay: -1,
+        stagger: .2
+    })
+
+    tl.from("#herofooter", {
+        y: -10,
+        opacity: 0,
+        ease: Expo.easeInOut,
+        delay: -1,
+        duration: 1.5,
+    })
+    
+}
+
+mouseMoveAnim();
+firstPageAnim();
+circleChaptaKaro();
